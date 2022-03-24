4453dd9be0376edce09898cc102c3f40dc8d6e1b
swu3025 <65374120+swu3025@users.noreply.github.com>
2022-03-23 21:16:25 -0700

    Add files via upload

--- functions/MixAudioButtonPushed.m
+++ functions/MixAudioButtonPushed.m
@@ -1,8 +1,9 @@
-%Samuel's Audio Mixer
 % Button pushed function: MixAudioButton
 function MixAudioButtonPushed(app, event)
     %mixes two audio signals to create a new signal and graph it
-    output = app.yGraph + app.y2Graph;
-    hold(app.UIAxes,'off')
-    plot(app.UIAxes,output)
+    if size(app.yGraph) == size(app.y2Graph) 
+        output = app.yGraph + app.y2Graph;
+        hold(app.UIAxes,'off')
+        plot(app.UIAxes,output)
+    end
 end
