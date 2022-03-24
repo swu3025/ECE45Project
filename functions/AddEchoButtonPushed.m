41f4f4c207bcdbf2c68cf274f1c4a96545d64e79
swu3025 <65374120+swu3025@users.noreply.github.com>
2022-03-23 20:47:39 -0700

    Add files via upload

---
+++ functions/AddEchoButtonPushed.m
@@ -0,0 +1,27 @@
+% Button pushed function: AddEchoButton
+function AddEchoButtonPushed(app, event)
+%
+%Modelling echo
+%
+timelag = 0.23;
+Fs = 1024;
+delta = round(Fs*timelag);
+alpha = 0.5;
+
+orig = [app.nGraph;zeros(delta,1)];
+echo = [zeros(delta,1);app.nGraph]*alpha;
+
+app.mtEcho = orig + echo;
+
+app.t = (0:length(app.mtEcho)-1)/Fs;
+
+[app.Rmm,app.lags] = xcorr(app.mtEcho,'unbiased');
+
+app.Rmm = app.Rmm(app.lags>0);
+app.lags = app.lags(app.lags>0);
+hold(app.UIAxes,'off')
+plot(app.UIAxes,app.lags/Fs,app.Rmm)
+
+app.echoAdded = true;
+
+end
